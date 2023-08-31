916aa9bfb6684a6e3819a305db0936ee394c832a
Hybusa <alkouzne007@gmail.com>
2023-08-31 11:29:12 +0300

    initial commit

---
+++ src/main/java/com/github/hybusa/TestTaskMSocial/TestTaskMSocialApplication.java
@@ -0,0 +1,13 @@
+package com.github.hybusa.TestTaskMSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskMSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskMSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hybusa/TestTaskMSocial/TestTaskMSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hybusa.TestTaskMSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskMSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
